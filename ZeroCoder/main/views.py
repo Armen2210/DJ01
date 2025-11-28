ed445deedf490c1c2ee8a325b1d798aa6f96df49
Армен Амбарцумян <armen0858@gmail.com>
2025-11-28 10:33:41 +0200

    updates

--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+
+def new(request):
+    return HttpResponse("<h1>Это страница new</h1>")
+
+def data(request):
+    return HttpResponse("<h1>Это страница data</h1>")
+
+def test(request):
+    return HttpResponse("<h1>Это страница test</h1>")
