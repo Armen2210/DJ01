cad5e91e483cdfbb6c9888a7738adf5a7f320ebe
Армен Амбарцумян <armen0858@gmail.com>
2025-12-11 18:07:04 +0300

    updates

--- ZeroCoder/news/models.py
+++ ZeroCoder/news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class News_post(models.Model):
+    title = models.CharField('Название новости', max_length=100)
+    short_description = models.CharField('Краткое описание новости', max_length=255)
+    text = models.TextField('Полное описание новости')
+    name = models.CharField('Имя автора', max_length=100)
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+
